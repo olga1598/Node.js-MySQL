fb9dd8327758614fa816734b0aafbddc2c32c0c6
Olga <olga.repnitsyna@gmail.com>
2019-03-11 05:05:30 -0400

    Challenge 2 is done

--- bamazonManager.js
+++ bamazonManager.js
@@ -48,6 +48,7 @@ function start() {
     });
 };
 
+//Function that refers to another function depending on user choice.
 function doChoice(options) {
 
     switch(options) 
@@ -62,15 +63,16 @@ function doChoice(options) {
             break;
         case "Add to Inventory":
             //console.log("In the addInventory function");
-            // addInventory();
+            addInventory();
             break;
         case "Add New Product":
             //console.log("In the addNewProduct function");
-            // addNewProduct();
+            addNewProduct();
             break;
     }
 };
 
+//Display the whole available list of items for sale
 function viewProduct() {
     connection.query("SELECT * FROM products", function(err,res) {
         console.log("------------------------------");
@@ -81,10 +83,14 @@ function viewProduct() {
 
         };
         console.log("------------------------------");
+        console.log("What else do you want to check out?");
+        console.log("------------------------------");
+        start();
 
     });
 };
 
+//Checking what items has the stock quantity less then 5
 function lowInventory() {
     connection.query("SELECT * FROM products", function(err, res) {
 
@@ -98,10 +104,113 @@ function lowInventory() {
             }
         };
         console.log("------------------------------");
+        console.log("Do you want to check out something else?");
+        console.log("------------------------------");
+        start();
     });
 };
 
+//Add to existing inventory more 
+function addInventory() {
+    connection.query("SELECT * FROM products", function(err, res) {
 
+    console.log("Please enter the ID of the item that you want to add more");
+    console.log("------------------------------");
+    for (var i = 0; i < res.length; i++) {
+        console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+
+    };
+    console.log("------------------------------");
+
+    inquirer.prompt ([
+        {
+            type: "input",
+            name: "pickItemID",
+            message: "Please enter the ID of the item that you want to add more"
+        },
+        {
+            type: "input",
+            name: "addMoreID",
+            message: "Please enter the quantity you want to add more"
+        },
+
+    ]).then(function(answer) {
+        //console.log(answer.addMoreID);
+        var query = "SELECT id, product_name, stock_quantity FROM products WHERE ?";
+        connection.query(query, {id: answer.pickItemID}, function(err,res) {
+            // console.log(answer.pickItemID);
+            // console.log(res);
+            // console.log(res[0].stock_quantity);
+            console.log("------------------------------");
+            console.log("The product you've picked: " + res[0].id + " | " + res[0].product_name + " | " + res[0].stock_quantity);
+            console.log("------------------------------");
+        })
+        connection.query("UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: parseInt(answer.addMoreID) + (res[parseInt(answer.pickItemID) - 1]).stock_quantity
+            },
+            { 
+                id: answer.pickItemID
+            }
+        ], function(error) {
+            if (error) throw err;
+            console.log("stock quantity have been updated");
+        });
+        connection.query("SELECT * FROM products", function(err,res) {
+            console.log("------------------------------");
+            console.log("The updated quantity of | " + (res[answer.pickItemID - 1]).product_name + " | is | " + (res[answer.pickItemID - 1]).stock_quantity);
+            console.log("------------------------------");
+            //console.log(res[answer.pickItemID - 1]);
+            console.log("==============================");
+            start();
+        });
+    });
+    });
+};
+
+//We are going to add a new product to our list of products
+function addNewProduct() {
+    inquirer.prompt ([
+        {
+            type: "input",
+            message: "What is the name of the new product?",
+            name: "productname"
+        },
+        {
+            tepe: "input",
+            message: "What department do you want to put it in?",
+            name: "department"
+        },
+        {
+            type: "input",
+            message: "What is the price for one piece of your new item in $?",
+            name: "price"
+        },
+        {
+            type: "input",
+            message: "How many pieces of your new item will be available for sale?",
+            name: "quantity"
+        }
+    ]).then(function(answer) {
+        console.log("You've entered: " + answer.productname + " | " + answer.department + " | " + answer.price + " | " + answer.quantity);
+        
+        //we add all the info to our table
+        connection.query("INSERT INTO products SET ?",
+        {
+            product_name: answer.productname,
+            department_name: answer.department,
+            price: answer.price,
+            stock_quantity: answer.quantity
+        }, function(err) {
+            if (err) throw err;
+            console.log(" product inserted!\n");
+            viewProduct();
+        });
+    })
+};
+
+//==============================================================
 
 
 
